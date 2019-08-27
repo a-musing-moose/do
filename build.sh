51810c7388fbd85f79ba79c32e768028e31b7fda
Jonathan Moss <jmoss@commoncode.io>
2019-08-27 22:08:09 +1000

    wrap it up as a script

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+    EXECUTABLE_NAME="do-Linux"
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    EXECUTABLE_NAME="do-Darwin"
+fi
+pip3 install -r requirements.txt
+pyinstaller -F --hidden-import=utils "$EXECUTABLE_NAME"
