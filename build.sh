092c61a3bec6499480e8b850b771d7b7ecce647e
Jonathan Moss <xirisr@gmail.com>
2020-01-13 14:37:21 +1100

    releasing

--- build.sh
+++ build.sh
@@ -1,9 +1,3 @@
 #!/usr/bin/env bash
-
-if [[ "$OSTYPE" == "linux-gnu" ]]; then
-    EXECUTABLE_NAME="do-Linux"
-elif [[ "$OSTYPE" == "darwin"* ]]; then
-    EXECUTABLE_NAME="do-Darwin"
-fi
 pip3 install -r requirements.txt
-pyinstaller -F -n "$EXECUTABLE_NAME" --hidden-import=do.utils entrypoint.py
+pyinstaller -F -n "do" --hidden-import=do.utils entrypoint.py
